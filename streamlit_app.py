fade57d5f02dabe3de7b943e293f5c0caaf5a283
Dhnamgh <danhchosv@gmail.com>
2025-10-09 21:26:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -289,14 +289,14 @@ elif choice == "📱 Mã QR xe":
 elif choice == "📤 Xuất ra Excel":
     st.subheader("📤 Tải danh sách xe dưới dạng Excel")
     output = BytesIO()
-with pd.ExcelWriter(output, engine='openpyxl') as writer:
-    df.to_excel(writer, index=False, sheet_name='DanhSachXe')
-    writer.close()
-    processed_data = output.getvalue()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='DanhSachXe')
+        writer.close()
+        processed_data = output.getvalue()
 
-st.download_button(
-    label="📥 Tải Excel",
-    data=processed_data,
-    file_name="DanhSachXe.xlsx",
-    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-)
+    st.download_button(
+        label="📥 Tải Excel",
+        data=processed_data,
+        file_name="DanhSachXe.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
